8b0c2f06e549d54e958db938d12a3a393016b6d7
katia <katiandkate@gmail.com>
2020-06-20 19:11:51 +0200

    по 3 часть, тесты работают. Попытка исправить ошибки

--- src/Transliterator.java
+++ src/Transliterator.java
@@ -1,12 +1,10 @@
 import java.io.IOException;
-import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Transliterator {
     public char[] r = {'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
     public String[] e = {"a", "b", "v", "g", "d", "e", "e", "zh", "z", "i", "i", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "kh", "ts", "ch", "sh", "shch", "ie", "y", "", "e", "iu", "ia"};
-
 
     public Transliterator(char[] r, String[] e) {
         this.r = r;
@@ -42,12 +40,6 @@ public class Transliterator {
     }
 
     public void translateFile(String name1, String name2) throws IOException {
-        String f = Files.readString(Paths.get(name1));
-        String[] l = f.split(" ");
-        try (PrintStream out = new PrintStream(name2, "utf8")) {
-            for (int i = 0; i < l.length; i++) {
-                out.print(translate(l[i]) + " ");
-            }
-        }
+        Files.writeString(Paths.get(name2), translate(Files.readString(Paths.get(name1))));
     }
 }
