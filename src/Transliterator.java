7f3270092a886da75b3a63712913273018598907
katia <katiandkate@gmail.com>
2020-06-20 13:41:58 +0200

    Сделана задача до 2.1, тесты работают

--- src/Transliterator.java
+++ src/Transliterator.java
@@ -7,11 +7,16 @@ public class Transliterator {
 
         }
         //Символы первого массива должны превращаться в соответствующие
-//цепочки символов второго массива.
+        //цепочки символов второго массива.
     String translateOneSymbol(char symbol){
-        for(int i = 0; i < r.length; i++) {
+        String testing = String.valueOf(symbol).toLowerCase();
+        for (int i = 0; i < r.length; i++) {
         if (r[i] == symbol)
             return e[i];
+        else if (testing.charAt(0) == r[i]){
+            String[] l = e[i].split("");
+            return e[i].replaceAll(l[0], l[0].toUpperCase());
+            }
         }
         return String.valueOf(symbol);
   }
